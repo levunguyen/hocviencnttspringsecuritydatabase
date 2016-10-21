97be00518f57057d641969f76586f3ab8cc0bed9
nguyenle <nguyen.levu@smartdev.vn>
2016-10-21 16:43:47 +0700

    web view

--- src/main/java/org/hocviencntt/service/UserServiceImpl.java
+++ src/main/java/org/hocviencntt/service/UserServiceImpl.java
@@ -7,7 +7,9 @@ import org.hocviencntt.repository.RoleRepository;
 import org.hocviencntt.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
 
 	@Autowired
